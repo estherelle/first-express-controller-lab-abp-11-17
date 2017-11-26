6918a09c5ec95ba9489ebcaba59d949022af67bb
Esther Liao <erliao@princeton.edu>
2017-11-26 21:32:07 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -2,13 +2,6 @@ var express = require('express')
 var app = express()
 
 // Require controller modules
-var siteController = require('../controllers/SiteController')
-
-app.get('/', '../'{
-  resp.render('site/Index', 'siteController' {
-    "welcomePhrase": "Hello, World from Express and EJS!"
-  })
-})
 
 
 module.exports = app
