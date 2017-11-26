2341498b8ef12cc1a96acbb92fb4c125e70cf63f
Esther Liao <erliao@princeton.edu>
2017-11-26 21:35:25 +0000

    automatically backed up by learn

---
+++ controllers/SiteController.js
@@ -0,0 +1,24 @@
+module.exports.controller = function(app) {
+
+/**
+ * a home page route
+ */
+  app.get('/', function(req, res) {
+      // any logic goes here
+      res.render('../views/site/index')
+  });
+
+/**
+ * About page route
+ */
+  app.get('/about', function(req, res) {
+      // any logic goes here
+      res.render('../views/site/about')
+  });
+
+  app.get('/contact', function(req, res) {
+      // any logic goes here
+      res.render('../views/site/contact')
+  });
+
+}
