c2239c60aef8633f0e3bf52cbdb338e6969481b7
Esther Liao <erliao@princeton.edu>
2017-11-26 21:31:48 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 var express = require('express')
 var app = express()
 
+// Require controller modules
+var siteController = require('../controllers/SiteController')
+
+app.get('/', '../'{
+  resp.render('site/Index', 'siteController' {
+    "welcomePhrase": "Hello, World from Express and EJS!"
+  })
+})
 
 
 module.exports = app
-
-
-
-
-
-
-
-
-
-
